ed985fb00c00ac29b7d35bd243455fab5f51bbc7
edulyan <edgarkhurshudyan17@gmail.com>
2022-08-03 21:38:03 +0300

    try

--- src/config/typeORM.config.ts
+++ src/config/typeORM.config.ts
@@ -2,15 +2,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const ConfigPG: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT) || 5432,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
+  host: 'postgresMovieSite',
+  port: 5432,
+  username: 'edgar',
+  password: '20012002',
   database: 'movie-website',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: false,
+  synchronize: true,
   migrationsRun: false,
-  logging: true,
+  logging: false,
   migrations: [__dirname + '/../**/migrations/*{.ts,.js}'],
   cli: {
     migrationsDir: 'src/migrations',
